6b02a4ef8f132f2d18f5bd5e1b4e707c786cc679
Cbayy <cbayne99@gmail.com>
2023-06-27 23:59:41 +1000

    Add colourscheme and autocomplete for lua

--- nvim/lua/plugin-configs/lspconfig.lua
+++ nvim/lua/plugin-configs/lspconfig.lua
@@ -3,5 +3,28 @@ require('mason-lspconfig').setup({
 	ensure_installed = { "lua_ls" , "rust_analyzer" }
 })
 
-require('lspconfig').lua_ls.setup {}
+local cmp = require('cmp')
+
+cmp.setup({
+	sources = {
+		{ name = 'nvim_lsp' },
+	},
+})
+
+
+vim.api.nvim_set_keymap('i', '<C-Space>', 'v:lua.require("cmp").complete()', { expr = true, noremap = true })
+
+require('lspconfig').lua_ls.setup {
+	lspconfig = {
+		cmd = { 'lua-language-server' },
+		on_attach = require('lsp').common_on_attach,
+		settings = {
+			Lua = {
+				completion = {
+					keywordSnippet = 'Disable',
+				},
+			},
+		},
+	},
+}
 require('lspconfig').rust_analyzer.setup {}
