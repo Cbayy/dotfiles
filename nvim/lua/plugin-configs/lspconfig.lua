2cca9681093c6d2a024ca8974d01401494a86119
Cbayy <cbayne99@gmail.com>
2023-06-28 00:20:50 +1000

    Add snippets and change autocomplete for lua

--- nvim/lua/plugin-configs/lspconfig.lua
+++ nvim/lua/plugin-configs/lspconfig.lua
@@ -6,25 +6,31 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 
 cmp.setup({
-	sources = {
-		{ name = 'nvim_lsp' },
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-o>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
 	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	}),
 })
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-vim.api.nvim_set_keymap('i', '<C-Space>', 'v:lua.require("cmp").complete()', { expr = true, noremap = true })
+--vim.api.nvim_set_keymap('i', '<C-Space>', 'v:lua.require("cmp").complete()', { expr = true, noremap = true })
 
 require('lspconfig').lua_ls.setup {
-	lspconfig = {
-		cmd = { 'lua-language-server' },
-		on_attach = require('lsp').common_on_attach,
-		settings = {
-			Lua = {
-				completion = {
-					keywordSnippet = 'Disable',
-				},
-			},
-		},
-	},
+	capabilities = capabilities
 }
 require('lspconfig').rust_analyzer.setup {}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,6 +6,10 @@ require('packer').startup(function(use)
 		'williamboman/mason-lspconfig.nvim',
 		'neovim/nvim-lspconfig',
 	}
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'L3MON4D3/LuaSnip'
+	use 'folke/tokyonight.nvim'
 end)
 
 local plugin_configs_path = vim.fn.stdpath('config') .. '/lua/plugin-configs'
@@ -14,3 +18,5 @@ for _, filename in ipairs(vim.fn.readdir(plugin_configs_path)) do
     local plugin_config = plugin_configs_path .. '/' .. filename
     require('plugin-configs/' .. vim.fn.fnamemodify(filename, ':r'))
 end
+
+vim.cmd 'colorscheme tokyonight-moon'
